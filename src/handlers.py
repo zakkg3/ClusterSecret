ba99b32b841cb5fcebdc44add791cfe29b8719b2
Nico <zakkg3@gmail.com>
2020-04-27 23:19:48 +0200

    working handler first step

--- src/handlers.py
+++ src/handlers.py
@@ -3,22 +3,31 @@ import re
 import yaml
 from kubernetes import client, watch, config
 
-@kopf.on.create('clustersecret.io', 'v1', 'clustersecret')
+@kopf.on.create('clustersecret.io', 'v1', 'clustersecrets')
 def create_fn(spec,logger=None,body=None,**kwargs):
-    logger.debug (spec)
+    logger.debug (f'spec: {spec}')
+    logger.debug ('----------')
+    logger.debug (f'body: {body}')
+    logger.debug ('###################2')
+    logger.debug (f"name: {body['metadata']['name']}")
     
     try:
-        nss = body.get('matchNamespace')
+        matchNamespace = body.get('matchNamespace')
     except KeyError:
-        nss = '*'
+        matchNamespace = '*'
         logger.debug("matching all namespaces.")
-    logger.debug(f'nss: {nss}')
+    logger.debug(f'nss: {matchNamespace}')
     try:
-        avoid = body.get('avoidNamespaces')
+        avoidNamespaces = body.get('avoidNamespaces')
     except KeyError:
-        avoid = ''
+        avoidNamespaces = ''
         logger.debug("not avoiding namespaces")
-    
+    try:
+        name = body['metadata']['name']
+    except KeyError:
+        logger.debug("No name ?")
+        return 1 #raise something here
+        
     sync_secret(name,matchNamespace,avoidNamespaces)
     return True
 
@@ -28,8 +37,7 @@ def create_fn(spec,logger=None,body=None,**kwargs):
 def sync_secret(name,matchNamespace,avoidNamespaces):
     # config.load_kube_config()
     v1 = client.CoreV1Api()
-    nss = v1.list_namespace()
+    nss = v1.list_namespace().items
     
-    for ns in nss.items():
-        print (ns.metadata.name)   
-    
+    for ns in nss:
+        print (ns.metadata.name)
