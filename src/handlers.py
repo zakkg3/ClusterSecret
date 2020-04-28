19ce55a563a11a478e4a0594ad2221d671347cc9
Nico <zakkg3@gmail.com>
2020-04-28 21:56:29 +0200

    on delete, delete all secrets, on data update, update all secrets

--- src/handlers.py
+++ src/handlers.py
@@ -2,6 +2,35 @@ import kopf
 import re
 from kubernetes import client, config
 
+
+@kopf.on.delete('clustersecret.io', 'v1', 'clustersecrets')
+def on_delete(spec,body,name,logger=None, **_):
+    syncedns = body['status']['create_fn']['syncedns']
+    v1 = client.CoreV1Api()
+    for ns in syncedns:
+        logger.info(f'deleting secret {name} from naespace {ns}')
+        v1.delete_namespaced_secret(name,ns)
+        
+@kopf.on.field('clustersecret.io', 'v1', 'clustersecrets', field='data')
+def on_field_data(old, new, body,name,logger=None, **_):
+    logger.debug('----------------')
+    logger.info(f'Data changed: {old} -> {new}')
+    syncedns = body['status']['create_fn']['syncedns']
+    v1 = client.CoreV1Api()
+    for ns in syncedns:
+        logger.info(f'patching secret {name} in ns {ns}')
+        metadata = {'name': name, 'namespace': ns}
+        api_version = 'v1'
+        kind = 'Secret'
+        data = new
+        body = client.V1Secret(api_version, data , kind, metadata, type='kubernetes.io/tls')
+        # response = v1.patch_namespaced_secret(name,ns,body)
+        response = v1.replace_namespaced_secret(name,ns,body)
+        logger.debug(response)
+        
+    
+
+@kopf.on.resume('clustersecret.io', 'v1', 'clustersecrets')
 @kopf.on.create('clustersecret.io', 'v1', 'clustersecrets')
 def create_fn(spec,logger=None,body=None,**kwargs):
 
@@ -23,7 +52,7 @@ def create_fn(spec,logger=None,body=None,**kwargs):
         logger.debug (f"name: {name}")
     except KeyError:
         logger.debug("No name ?")
-        return 1 #raise something here
+        raise kopf.TemporaryError("can not get the name.")
 
     try:
         data = body.get('data')
@@ -38,7 +67,7 @@ def create_fn(spec,logger=None,body=None,**kwargs):
     #sync in all matched NS
     for ns in matchedns:
         create_secret(v1,logger,ns,name,data)
-    return True
+    return {'syncedns': matchedns}
 
 
 
@@ -72,9 +101,13 @@ def create_secret(v1,logger,namespace,name,data):
     api_version = 'v1'
     kind = 'Secret'
     body = client.V1Secret(api_version, data , kind, metadata, type='kubernetes.io/tls')
+    # kopf.adopt(body)
     try:
         api_response = v1.create_namespaced_secret(namespace, body)
     except client.rest.ApiException as e:
+        if e.reason == 'Conflict':
+            logger.info(f"Conflict creating the secret: It may be already a `{name}` secret in namespace: '{namespace}'")
+            return 0
         logger.error(f'Can not create a secret, it is base64 encoded? data: {data}')
         logger.error(f'Kube exception {e}')
         return 1
