86442d0ba3b5f67eded17744fb796ef95996b6be
Nico <zakkg3@gmail.com>
2023-09-04 18:35:08 +0200

    if matchnamespace is on the body do not nest list bro

--- src/csHelper.py
+++ src/csHelper.py
@@ -74,7 +74,7 @@ def get_ns_list(
     if 'matchNamespace' not in body:
         matchNamespace.append('.*')
     else:
-        matchNamespace.append(body['matchNamespace'])
+        matchNamespace=body['matchNamespace']
         
     logger.debug(f'Matching namespaces regex: {matchNamespace}')
 
