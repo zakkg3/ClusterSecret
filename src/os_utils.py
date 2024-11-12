0a64d6855c15f88092829eec07e0ac6c8d177d9a
Yoan Blanc <yoan.blanc@chuv.ch>
2024-11-12 11:54:51 +0100

    fixup! fixup! fixup! feat: allow to customize the blocked labels
    Signed-off-by: Yoan Blanc <yoan.blanc@chuv.ch>

--- src/os_utils.py
+++ src/os_utils.py
@@ -20,12 +20,11 @@ def get_replace_existing() -> bool:
 
 @cache
 def get_blocked_labels() -> list[str]:
-    blocked_labels = os.getenv('BLOCKED_LABELS')
+    if blocked_labels := os.getenv('BLOCKED_LABELS'):
+        return [label.strip() for label in blocked_labels.split(',')]
 
-    if not blocked_labels:
-        return BLOCKED_LABELS
+    return BLOCKED_LABELS
 
-    return [label.strip() for label in blocked_labels.split(',')]
 
 @cache
 def in_cluster() -> bool:
