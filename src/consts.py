b8cbf0fcdbb2f0fdd534b1123ffe3ab8b9c30cf8
Yoan Blanc <yoan.blanc@chuv.ch>
2024-11-12 09:10:42 +0100

    fix(charts): replace_existing wrongly indented
    Signed-off-by: Yoan Blanc <yoan.blanc@chuv.ch>

--- src/consts.py
+++ src/consts.py
@@ -10,4 +10,4 @@ VERSION_ANNOTATION = 'clustersecret.io/version'
 CLUSTER_SECRET_LABEL = "clustersecret.io"
 
 BLACK_LISTED_ANNOTATIONS = ["kopf.zalando.org", "kubectl.kubernetes.io"]
-BLACK_LISTED_LABELS = ["app.kubernetes.io"]
+BLACK_LISTED_LABELS = ["app.kubernetes.io", "argocd.argoproj.io/instance"]
