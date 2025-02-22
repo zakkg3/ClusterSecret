014cdcf49c8c83ce86ae0013fef71eae69d20bca
tymuraheiev <tymuraheiev@o-s-i.org>
2025-02-22 23:24:53 +0200

    Added new unit test for namespace deletion event Removed unnecessary unit test

--- src/tests/test_handlers.py
+++ src/tests/test_handlers.py
@@ -116,143 +116,6 @@ class TestClusterSecretHandler(unittest.TestCase):
             {"key": "newvalue"},
         )
 
-    def test_on_field_data_ns_deleted(self):
-        """Don't fail the sync if one of the namespaces was deleted.
-        """
-
-        mock_v1 = Mock()
-
-        def read_namespaced_secret(name, namespace, **kwargs):
-            if namespace == "myns2":
-                # Old data in the namespaced secret of the myns namespace.
-                return V1Secret(
-                    api_version='v1',
-                    data={"key": "oldvalue"},
-                    kind='Secret',
-                    metadata=create_secret_metadata(
-                        name="mysecret",
-                        namespace="myns2",
-                    ),
-                    type="Opaque",
-                )
-            else:
-                # Deleted namespace.
-                raise ApiException(status=404, reason="Not Found")
-
-        mock_v1.read_namespaced_secret = read_namespaced_secret
-
-        create_namespaced_secret_called_count_for_ns2 = 0
-
-        def create_namespaced_secret(namespace, body, **kwargs):
-            if namespace == "myns2":
-                nonlocal create_namespaced_secret_called_count_for_ns2
-                create_namespaced_secret_called_count_for_ns2 += 1
-            else:
-                # Deleted namespace.
-                raise ApiException(status=404, reason="Not Found")
-
-        mock_v1.create_namespaced_secret = create_namespaced_secret
-
-        replace_namespaced_secret_called_count_for_ns2 = 0
-
-        def replace_namespaced_secret(name, namespace, body, **kwargs):
-            if namespace == "myns2":
-                nonlocal replace_namespaced_secret_called_count_for_ns2
-                replace_namespaced_secret_called_count_for_ns2 += 1
-                self.assertEqual(name, csec.name)
-
-                # Namespaced secret should be updated with the new data.
-                self.assertEqual(
-                    body.data,
-                    {"key": "newvalue"},
-                )
-
-                return V1Secret(
-                    api_version='v1',
-                    data=body.data,
-                    kind='Secret',
-                    metadata=create_secret_metadata(
-                        name="mysecret",
-                        namespace="myns2",
-                    ),
-                    type="Opaque",
-                )
-            else:
-                # Deleted namespace.
-                raise ApiException(status=404, reason="Not Found")
-
-        mock_v1.replace_namespaced_secret = replace_namespaced_secret
-
-        def read_namespace(name, **kwargs):
-            if name != "myns2":
-                # Deleted namespace.
-                raise ApiException(status=404, reason="Not Found")
-
-        mock_v1.read_namespace = read_namespace
-
-        patch_clustersecret_status = Mock()
-        patch_clustersecret_status.return_value = {
-            "metadata": {"name": "mysecret", "uid": "mysecretuid"},
-            "data": {"key": "newvalue"},
-            "status": {"create_fn": {"syncedns": ["myns2"]}},
-        }
-
-        # Old data in the cache.
-        csec = BaseClusterSecret(
-            uid="mysecretuid",
-            name="mysecret",
-            body={
-                "metadata": {"name": "mysecret", "uid": "mysecretuid"},
-                "data": {"key": "oldvalue"},
-                "status": {"create_fn": {"syncedns": ["myns1", "myns2"]}},
-            },
-            synced_namespace=["myns1", "myns2"],
-        )
-
-        csecs_cache.set_cluster_secret(csec)
-
-        # New data coming into the callback.
-        new_body = {
-            "metadata": {"name": "mysecret", "uid": "mysecretuid"},
-            "data": {"key": "newvalue"},
-            "status": {"create_fn": {"syncedns": ["myns1", "myns2"]}},
-        }
-
-        with patch("handlers.v1", mock_v1), \
-             patch("handlers.patch_clustersecret_status", patch_clustersecret_status):
-            on_field_data(
-                old={"key": "oldvalue"},
-                new={"key": "newvalue"},
-                body=new_body,
-                meta=kopf.Meta({"metadata": {"name": "mysecret"}}),
-                name="mysecret",
-                uid="mysecretuid",
-                logger=self.logger,
-                reason="update",
-            )
-
-        # Namespaced secret should be updated with the new data.
-        self.assertEqual(replace_namespaced_secret_called_count_for_ns2, 1)
-        self.assertEqual(create_namespaced_secret_called_count_for_ns2, 0)
-
-        # The namespace should be deleted from the syncedns status of the clustersecret.
-        patch_clustersecret_status.assert_called_once_with(
-            logger=self.logger,
-            name=csec.name,
-            new_status={'create_fn': {'syncedns': ["myns2"]}},
-            custom_objects_api=custom_objects_api,
-        )
-
-        # Namespace should be deleted from the cache.
-        self.assertEqual(
-            csecs_cache.get_cluster_secret("mysecretuid").body.get("status"),
-            {"create_fn": {"syncedns": ["myns2"]}},
-        )
-        self.assertEqual(
-            csecs_cache.get_cluster_secret("mysecretuid").synced_namespace,
-            ["myns2"],
-        )
-
     def test_create_fn(self):
         """Namespace name must be correct in the cache.
         """
@@ -319,6 +182,7 @@ class TestClusterSecretHandler(unittest.TestCase):
                 namespace_watcher(
                     logger=self.logger,
                     meta=kopf.Meta({"metadata": {"name": "myns"}}),
+                    reason="create",
                 )
             )
 
@@ -341,6 +205,54 @@ class TestClusterSecretHandler(unittest.TestCase):
         self.assertCountEqual(
             csecs_cache.get_cluster_secret("mysecretuid").synced_namespace,
             ["default", "myns"],
+        )
+
+    def test_ns_delete(self):
+        """Deleted namespace must be removed from cluster secret 'status.create_fn.syncedns' filed.
+        """
+
+        mock_v1 = Mock()
+
+        # Define the predefined list of namespaces you want to use in the test (after namespace deletion)
+        predefined_nss = [Mock(metadata=V1ObjectMeta(name=ns)) for ns in ["default"]]
+
+        # Configure the mock's behavior to return the predefined namespaces when list_namespace is called
+        mock_v1.list_namespace.return_value.items = predefined_nss
+
+        patch_clustersecret_status = Mock()
+
+        # The list of synced namespaces here are before namespace deletion handler is called
+        csec = BaseClusterSecret(
+            uid="mysecretuid",
+            name="mysecret",
+            body={"metadata": {"name": "mysecret"}, "data": "mydata"},
+            synced_namespace=["default", "myns"],
+        )
+
+        csecs_cache.set_cluster_secret(csec)
+
+        with patch("handlers.v1", mock_v1), \
+             patch("handlers.patch_clustersecret_status", patch_clustersecret_status):
+            asyncio.run(
+                namespace_watcher(
+                    logger=self.logger,
+                    meta=kopf.Meta({"metadata": {"name": "myns"}}),
+                    reason="delete",
+                )
+            )
+
+        # The syncedns status of the clustersecret should not contains deleted namespace.
+        patch_clustersecret_status.assert_called_once_with(
+            logger=self.logger,
+            name=csec.name,
+            new_status={'create_fn': {'syncedns': ["default"]}},
+            custom_objects_api=custom_objects_api,
+        )
+
+        # The deleted namespace should not be in the cache.
+        self.assertCountEqual(
+            csecs_cache.get_cluster_secret("mysecretuid").synced_namespace,
+            ["default"],
         )
 
     def test_startup_fn(self):
